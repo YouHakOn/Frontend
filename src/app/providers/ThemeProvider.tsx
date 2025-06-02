7de7871289999ce42de6239911079abb7012c669
jameslol417 <jameslol4177@gmail.com>
2025-06-02 17:57:09 +0900

    working on mantine themes
    - attempt on integrating SSR friendly theming with mantine

--- src/app/providers/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider.tsx
@@ -1,12 +1,14 @@
+// 'use client'
 import React from 'react'
-import { createTheme, MantineProvider } from '@mantine/core'
+import { createTheme, MantineProvider, rem } from '@mantine/core'
+import classes from '@app/providers/mantine.module.css'
 
 const theme = createTheme({
   primaryColor: 'primary',
   colors: {
     'primary': [
-      '#EAFAF4', '#D1F7E3', '#A9F0CC', '#80EAB5', '#00C471', 
-      '#009D5E', '#009D5E', '#009D5E', '#00784D', '#004D2D'
+      '#EAFAF4', '#D1F7E3', '#A9F0CC', '#80EAB5', '#4DDA9A', 
+      '#00C471', '#009D5E', '#009784D', '#004D2D', '#00331D'
     ],
     'secondary': [
       '#F5FAFF', '#DFF0FE', '#B3D9FA', '#86BFF5', '#5AA0F0',
@@ -15,57 +17,34 @@ const theme = createTheme({
     'gray': [
       '#F8F9FA', '#F1F3F5', '#E9ECEF', '#DEE2E6', '#DEE2E6', 
       '#ADB5BD', '#868E96', '#495057', '#343A40', '#212529'],
-    'line': [
-      '#CED4DA', '#ADB5BD', '#E9ECEF',
-      '#CED4DA', '#ADB5BD', '#E9ECEF',
-      '#CED4DA', '#ADB5BD', '#E9ECEF', '#CED4DA'
-    ],
-    'bg': [
-      '#FFFFFF', '#F8F9FA', '#F1F3F5',
-      '#FFFFFF', '#F8F9FA', '#F1F3F5',
-      '#FFFFFF', '#F8F9FA', '#F1F3F5', '#FFFFFF'
-    ],
-    'text': [
-      '#212529', '#343A40', '#868E96', '#ADB5BD', '#FFFFFF',
-      '#212529', '#343A40', '#868E96', '#ADB5BD', '#FFFFFF'
-    ],
-    'icon': [
-      '#343A40', '#868E96', '#CED4DA',
-      '#343A40', '#868E96', '#CED4DA',
-      '#343A40', '#868E96', '#CED4DA', '#343A40'
-    ],
-    'divider': [
-      '#DEE2E6', '#ADB5BD', '#DEE2E6', '#ADB5BD', '#DEE2E6',
-      '#ADB5BD', '#DEE2E6', '#ADB5BD', '#DEE2E6', '#ADB5BD'
-    ],
-    'status': [
-      '#FF5C45', '#FFC83C', '#33D486', '#3789FF',
-      '#FF5C45', '#FFC83C', '#33D486', '#3789FF', '#FF5C45', '#FFC83C'
-    ],
   },
   fontFamily: 'Pretendard',
   fontSizes: {
-    h0: '32px',
-    h1: '28px',
-    h2: '24px',
-    h3: '20px',
-    b1: '18px',
-    b2: '16px',
-    c1: '14px',
-    c2: '12px',
+    b1: rem('18px'),
+    b2: rem('16px'),
+    c1: rem('14px'),
+    c2: rem('12px'),
   },
   lineHeights: {
-    h0: '42px',
-    h1: '38px',
-    h2: '34px',
-    h3: '28px',
-    b1: '26px',
-    b2: '24px',
-    c1: '20px',
-    c2: '18px',
+    b1: rem('26px'),
+    b2: rem('24px'),
+    c1: rem('20px'),
+    c2: rem('18px'),
   },
-
-
+  headings: {
+    sizes: {
+      h1: { fontWeight: '700', fontSize: rem('32px'), lineHeight: rem('42px') }, // equiv to h0 in figma
+      h2: { fontWeight: '700', fontSize: rem('28px'), lineHeight: rem('38px') },
+      h3: { fontWeight: '700', fontSize: rem('24px'), lineHeight: rem('34px') },
+      h4: { fontWeight: '700', fontSize: rem('20px'), lineHeight: rem('28px') },
+    }
+  },
+  components: {
+    Button: {
+      classNames: classes
+    }
+  },
+  defaultRadius: 6,
 });
 
 interface PropType {
--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,39 +1,29 @@
 'use client'
 
-import Link from "next/link"
-
-import { useMantineTheme } from "@mantine/core"
-import cls from "./HomePage.module.css"
+import { Button, useMantineTheme } from '@mantine/core'
 
 const HomePage = () => {
-	const theme = useMantineTheme();
+  const theme = useMantineTheme()
 
-	return (
-		<div className={cls.homepage}>
-			{/* test */}
-			<p 
-				style={{
-					backgroundColor: theme.colors.primary[2]
-				}}
-			>
-				Hello from{" "}
-				<Link
-					href="https://github.com/yunglocokid"
-					target="_blank"
-					className={cls.link}
-				>
-					yunglocokid
-				</Link>
-			</p>
-			<pre className={cls.hint} data-testid="hint-code">
-				You can edit <span className={cls.path}>src/pages/HomePage</span> to
-				start {"<3"}!<br />
-				<small className={cls.testHint}>
-					You can also test your application using Jest :D. Try it!
-				</small>
-			</pre>
-		</div>
-	)
+  return (
+    <div>
+      {/* test */}
+      <p
+        style={{
+          backgroundColor: theme.colors.primary[2]
+        }}
+      >
+        <Button
+          style={{ width: '116px' }}
+          variant="filled"
+          color={theme.colors.primary[5]}
+          size="lg"
+        >
+          로그인
+        </Button>
+      </p>
+    </div>
+  )
 }
 
 export default HomePage
